371000d2522cf75637f5793d79e9e7a7a1776251
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-26 17:46:55 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// BU YERNI TUZAT
 export default defineConfig({
-  base: '/landing-page-for-itcreative/', // <<< BU YERNI QO‘SH
+  base: '/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
