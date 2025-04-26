f2d0c763ca808593dc5e7d17f5f849f9ebf5aa05
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-26 16:57:16 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  base: isGithubPages ? '/landing-page-for-itcreative/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
